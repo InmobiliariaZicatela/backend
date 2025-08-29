01ac6633e8269786e28a45e08bec25a36559f864
Inmobiliaria Zicatela <inmobiliariazicatela@gmail.com>
2025-08-29 18:52:01 +0200

    feat: configure server and admin URLs to use environment variables

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,11 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("STRAPI_SERVER_URL", "http://localhost:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  // Production security settings
-  ...(env("NODE_ENV") === "production" && {
-    url: env("PUBLIC_URL", "http://localhost:1337"),
-    proxy: true,
-    cron: {
-      enabled: false,
-    },
-  }),
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
 });
